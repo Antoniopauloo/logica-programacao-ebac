818e20035f300046e3e37554f3f8897f14e9c467
Antonio <antoniopaulocamarda@gmail.com>
2025-04-17 12:26:03 -0300

    Desafio

---
+++ RotinaDeAcordar.java
@@ -0,0 +1,13 @@
+public class RotinaDeAcordar {
+    public static void main(String[] args) {
+        System.out.println("Abrir os olhos");
+        System.out.println("Ficar deitado por 5 minutos");
+        System.out.println("Levantar da cama");
+        System.out.println("Ir ao banheiro");
+        System.out.println("Escovar os dentes");
+        System.out.println("Tomar banho");
+        System.out.println("Vestir roupa");
+        System.out.println("Tomar café da manhã");
+        System.out.println("Sair de casa");
+    }
+}
